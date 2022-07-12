d99d5eceece417ad4f6de18f6a0f0f52efe8f7ce
ArmandoB-A <blasarcej@gmail.com>
2022-07-12 02:43:02 -0500

    otro pequeño cambio

--- ProcesarRegistroU.php
+++ ProcesarRegistroU.php
@@ -14,9 +14,9 @@ if (isset($tabla[0])) {
     $row = mysqli_fetch_array($ejectQuery);
     echo "<br><h1>Cliente agregado con exito</h1>";
     echo "<br>";
-    echo "<a href='categorias.php?clave=0'>continuar conprando</a>";
+    echo "<a href='login.php'>iniciar sesion</a>";
     echo "<br>";
-    echo "<a href='generarFactura.php?folio=".$foliov."'>generar factura</a>";
+    echo "<a href='index.php'>ir al menú principal </a>";
 } else {
     echo "<h1>no se puedo agregar el usuario</h1><br>";
     echo "<a href='categorias.php?clave=0'>volver</a>";
