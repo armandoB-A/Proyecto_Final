0218bf907e4e03f7fa672726c1c2152484a90aa0
ArmandoB-A <blasarcej@gmail.com>
2022-07-12 02:07:04 -0500

    cambio

--- ProcesarRegistroU.php
+++ ProcesarRegistroU.php
@@ -2,6 +2,7 @@
 include("proyectoP/coneccion/conn.php");
 
 $queryP = "call ingresarUsuarios ('" . $_GET['nombre'] . "', '"  . $_GET['apemt'] . "',  '"  . $_GET['apept'] . "',  '"  . $_GET['direc'] . "',  '"  . $_GET['corre'] . "', '"  . $_GET['telef'] . "');";
+echo $queryP;
 $exeqQuery = mysqli_query($conn, $queryP);
 $tabla = mysqli_fetch_array($exeqQuery);
 if (isset($tabla[0])) {
