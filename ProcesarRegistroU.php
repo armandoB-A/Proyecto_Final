22063f8e03426e644f674e8ea7ec7f591a5d0876
ArmandoB-A <blasarcej@gmail.com>
2022-07-12 02:26:27 -0500

    pequeño cambio :z

--- ProcesarRegistroU.php
+++ ProcesarRegistroU.php
@@ -2,13 +2,14 @@
 include("proyectoP/coneccion/conn.php");
 
 $queryP = "call ingresarUsuarios ('" . $_POST['nombre'] . "', '"  . $_POST['apemt'] . "',  '"  . $_POST['apept'] . "',  '"  . $_POST['direc'] . "',  '"  . $_POST['corre'] . "', '"  . $_POST['telef'] . "');";
-echo $queryP;
+
 $exeqQuery = mysqli_query($conn, $queryP);
 $tabla = mysqli_fetch_array($exeqQuery);
 if (isset($tabla[0])) {
 
     include("proyectoP/coneccion/conn.php");
     $queryP = "CALL ingresarUSU('". $_POST['usern'] . "', '". $_POST['passw'] . "', ".$tabla[0].");";
+    echo $queryP;
     $ejectQuery = mysqli_query($conn, $queryP);
     $row = mysqli_fetch_array($ejectQuery);
     echo "<br><h1>Cliente agregado con exito</h1>";
